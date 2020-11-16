ae733c977812f49b13404b8c55b478a7d79f4932
mhgitrepo <mehedi1921@gmail.com>
2020-11-16 14:20:22 +0600

    Shopping Cart practice

---
+++ script.js
@@ -0,0 +1,130 @@
+//iPhone 11 128GB Black - Plus
+// document.getElementById("plus").addEventListener("click", function(){
+//     //Increment
+//     handleProductChange(true);
+// });
+
+//iPhone 11 128GB Black - Minus
+// document.getElementById("minus").addEventListener("click", function(){
+//     //Decrement
+//     handleProductChange(false);
+// });
+
+function handleProductChange (product, isIncrease){
+    const productInput = document.getElementById(product + "-count");
+    const productCount = parseInt(productInput.value);
+    // const valueNewCount = valueCount - 1;
+    let productNewCount = productCount; // let = will change this value.
+    if(isIncrease == true){
+        productNewCount = productCount + 1;
+    }
+    if(isIncrease == false && productCount > 0){
+        productNewCount = productCount - 1;
+    }
+    productInput.value = productNewCount;
+    // const unitTotal = productNewCount * 1219;
+    let productTotal = 0;
+    if(product == 'phone'){
+        productTotal = productNewCount * 1219;
+    }
+    if(product == 'case'){
+        productTotal = productNewCount * 59;
+    }
+
+    document.getElementById(product + "-total").innerText = '$' + productTotal;
+}
+
+// function handlePhoneChange(isIncrease){
+//     const valueInput = document.getElementById("quantity");
+//     const valueCount = parseInt(valueInput.value);
+//     // const valueNewCount = valueCount - 1;
+//     let valueNewCount = valueCount; // let = will change this value.
+//     if(isIncrease == true){
+//         valueNewCount = valueCount + 1;
+//     }
+//     if(isIncrease == false && valueCount > 0){
+//         valueNewCount = valueCount - 1;
+//     }
+//     valueInput.value = valueNewCount;
+//     const unitTotal = valueNewCount * 1219;
+
+//     document.getElementById("price").innerText = '$' + unitTotal;
+// }
+
+// function handleCaseChange(isIncrease){
+//     const caseInput = document.getElementById("caseQuantity");
+//     const caseCount = parseInt(caseInput.value);
+//     // const caseNewCount = caseCount - 1;
+//     let caseNewCount = caseCount;
+//     if(isIncrease == true){
+//         caseNewCount = caseCount + 1;
+//     }
+//     if(isIncrease == false && caseCount > 0){
+//         caseNewCount = caseCount - 1;
+//     }
+//     caseInput.value = caseNewCount;
+//     const caseTotal = caseNewCount * 59;
+
+//     document.getElementById("casePrice").innerText = caseTotal;
+// }
+
+// document.getElementById("plus").addEventListener("click", function(){
+//     //Increment
+//     // const valueInput = document.getElementById("quantity").value;
+//     const valueInput = document.getElementById("quantity");
+//     const valueCount = parseInt(valueInput.value);
+//     const valueNewCount = valueCount + 1;
+//     // document.getElementById("quantity").value = valueNewCount;
+//     valueInput.value = valueNewCount;
+//     const unitTotal = valueNewCount * 1219;
+
+//     document.getElementById("price").innerText = unitTotal;
+// });
+
+// //iPhone 11 128GB Black - Minus
+// document.getElementById("minus").addEventListener("click", function(){
+//     //Decrement
+//     const valueInput = document.getElementById("quantity");
+//     const valueCount = parseInt(valueInput.value);
+//     const valueNewCount = valueCount - 1;
+//     valueInput.value = valueNewCount;
+//     const unitTotal = valueNewCount * 1219;
+
+//     document.getElementById("price").innerText = unitTotal;
+// });
+
+//iPhone 11 Silicone Case - Black - Plus
+// document.getElementById("casePlus").addEventListener("click", function(){
+//     //Increment
+//     handleProductChangeTwo(true);
+// });
+
+//iPhone 11 Silicone Case - Black - Minus
+// document.getElementById("caseMinus").addEventListener("click", function(){
+//     handleProductChangeTwo(false);
+// });
+
+
+
+
+// document.getElementById("casePlus").addEventListener("click", function(){
+//     //Increment
+//     const caseInput = document.getElementById("caseQuantity");
+//     const caseCount = parseInt(caseInput.value);
+//     const caseNewCount = caseCount + 1;
+//     caseInput.value = caseNewCount;
+//     const caseTotal = caseNewCount * 59;
+
+//     document.getElementById("casePrice").innerText = caseTotal;
+// });
+
+// //iPhone 11 Silicone Case - Black - Minus
+// document.getElementById("caseMinus").addEventListener("click", function(){
+//     const caseInput = document.getElementById("caseQuantity");
+//     const caseCount = parseInt(caseInput.value);
+//     const caseNewCount = caseCount - 1;
+//     caseInput.value = caseNewCount;
+//     const caseTotal = caseNewCount * 59;
+
+//     document.getElementById("casePrice").innerText = caseTotal;
+// });
